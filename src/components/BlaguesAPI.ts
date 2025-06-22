38d32e7785ddfd315567fe78d7c47d37eb3cd602
Malik Ibo <malik97un@gmail.com>
2025-06-22 19:39:49 +0200

    Ficher .env ajouter pour plus de sécurité

--- src/components/BlaguesAPI.ts
+++ src/components/BlaguesAPI.ts
@@ -1,6 +1,6 @@
 import BlaguesAPI from "blagues-api";
 const blagues = new BlaguesAPI(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzA5NDMxNzQxMjk4OTAxMDUyIiwibGltaXQiOjEwMCwia2V5Ijoia3NubkhJTzIyTFZYaUtrVzFzRGZlMUU2ZUlmQnNsOUZYRlJDa1RGWENlUXNBa1B2ejYiLCJjcmVhdGVkX2F0IjoiMjAyNS0wNi0yMlQxNjo0ODo0MyswMDowMCIsImlhdCI6MTc1MDYxMDkyM30.QU7mAyNdy_RiZNq-3LxjZE9hr7mxEw4UU1R0MzvptKI"
+  import.meta.env.VITE_API_TOKEN!
 );
 
 export async function fetchRandomJoke() {
